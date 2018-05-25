e5f8e45e79f05b7897e217568e42398af5c89ad0
Harry Min <33393972+Avanta1@users.noreply.github.com>
2018-05-25 12:42:53 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,5 +1,21 @@
 class ScrapeWords {
-  constructor(courseUrl) {
+  
+  constructor(courseUrl, learnType, courseNum=0) {
+    var get_learnt_only = false;
+
+    if (learnType === 'review' || learnType === 'speedreview') {
+      get_learnt_only = true;
+    }
+
     document.location.href = courseUrl;
+
+    let subcourses = learnType === 'learn' ? courseUrl + courseNum + '/' : this.get_subcourses()
+    for (let i = 0; i < subcourses.length; i++) {
+      this.getwords(get_learnt_only);
+    }
   }
+  
+  get_subcourses() {
+  }
+  
 }
